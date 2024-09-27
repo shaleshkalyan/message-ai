02df658e94ec7dc79326e42f0a258677d5af111f
Shalesh Kalyan <shaleshkalyan123@gmail.com>
2024-09-28 04:56:39 +0530

    Dev : Authentication context created

--- src/global-context/AuthProvider.tsx
+++ src/global-context/AuthProvider.tsx
@@ -1,16 +1,53 @@
-'use client';
+"use client";
 
-import { createContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { cookies } from "next/headers";
 
-export default function AuthProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-    const authContext = createContext<boolean>(false);
-  return (
-    <>
-      {children}
-    </>
-  );
+interface AuthContextType {
+  session: sessionType | null;
+  checkLoggedIn: Function;
 }
+interface sessionType {
+  userName: string;
+  email: string;
+  userToken: number;
+  sessionExpiry: Date;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  session: null,
+  checkLoggedIn: () => {},
+});
+
+const router = useRouter();
+const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [session, setSession] = useState<sessionType | null>(null);
+  const checkLoggedIn = () => {
+    useEffect(() => {
+      const sessionData = cookies().get("userSession")?.value;
+      if (sessionData) {
+        let data = JSON.parse(sessionData);
+        setSession(data);
+      } else {
+        router.push("/login");
+      }
+    }, []);
+  };
+  const { Provider: AuthContextProvider } = AuthContext;
+  return (
+    <AuthContextProvider value={{ session, checkLoggedIn }}>
+      {children}
+    </AuthContextProvider>
+  );
+};
+
+export default AuthContextProvider;
+
+export const useAuthContext = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
