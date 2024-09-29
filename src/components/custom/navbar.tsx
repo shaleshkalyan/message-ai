a74e7651a47d687aef3c3fbd6ae0cc65a23c3988
Shalesh Kalyan <shaleshkalyan123@gmail.com>
2024-09-29 13:31:12 +0530

    Dev : Avatar and Navbar changes

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -15,6 +15,19 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 const Navbar = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -55,29 +68,50 @@ const Navbar = () => {
         </a>
         {authState.userName ? (
           <>
-            <Popover>
-              <PopoverTrigger asChild>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
                 <Avatar>
-                  <AvatarFallback className="bg-slate-100 text-black">
+                  <AvatarFallback className="bg-white text-black">
                     {authState.email.substring(0, 2).toUpperCase()}
                   </AvatarFallback>
                 </Avatar>
-              </PopoverTrigger>
-              <PopoverContent className="w-80">
-                <p className="text-sm text-muted-foreground">
-                  Welcome, {authState.userName}
-                </p>
-                <div className="grid gap-2">
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-60">
+                <DropdownMenuLabel>Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger className="h-8">
+                      Profile
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent>
+                        <DropdownMenuItem className="h-5">
+                          <span className="text-sm font-bold">Username : </span>
+                          {authState.userName}
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem className="h-5">
+                          <span className="text-sm font-bold">Email : </span>
+                          {authState.email}
+                        </DropdownMenuItem>
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
                   <Button
+                    size="icon"
+                    className="w-full text-sm h-5"
+                    variant={"ghost"}
                     onClick={logoutUser}
-                    className="w-full md:w-auto bg-slate-100 text-black"
-                    variant="outline"
                   >
                     Logout
                   </Button>
-                </div>
-              </PopoverContent>
-            </Popover>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </>
         ) : (
           <Button
