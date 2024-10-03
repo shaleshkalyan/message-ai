7cfe5981ad18bf860c4ebc9777556fdf901e214e
Shalesh Kalyan <shaleshkalyan123@gmail.com>
2024-10-03 23:39:13 +0530

    Fix : type fixed for id

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -14,6 +14,7 @@ import axios, { AxiosError } from "axios";
 import { Bell, Loader2, CopyIcon } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { string } from "zod";
 
 const Dashboard = () => {
   let { authState, authAction } = useAuthContext();
@@ -193,7 +194,7 @@ const Dashboard = () => {
         {messages.length > 0 ? (
           messages.map((messageData, index) => (
             <MessageCard
-              key={messageData._id}
+              key={(typeof(messageData._id) === "string") ? messageData._id : undefined}
               message={messageData}
               onDelete={handleDeleteMessage}
             />
